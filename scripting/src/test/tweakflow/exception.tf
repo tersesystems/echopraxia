e277b0518a28ed15aa5e10d006dca9ffd323fb7d
Will Sargent <will.sargent@gmail.com>
2022-01-16 19:58:16 -0800

    Add test for exception

--- scripting/src/test/tweakflow/exception.tf
+++ scripting/src/test/tweakflow/exception.tf
@@ -6,7 +6,7 @@ library echopraxia {
   # level: the logging level
   # fields: the dictionary of fields
   #
-  doc 'Evaluates if correlation_id matches given value'
+  doc 'Evaluates if exception matches'
   function evaluate: (string level, dict fields) ->
-     fields[:correlation_id] == "match";
+     fields[:exception][:message] == "testing";
 }
