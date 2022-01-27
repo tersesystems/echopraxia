7805cefb39cd9f90075f079ba0379d89f79cf29b
Will Sargent <will@tersesystems.com>
2022-01-27 07:30:04 -0800

    Add Log4J to README, more javadoc (#57)

--- api/src/main/java/com/tersesystems/echopraxia/ValueField.java
+++ api/src/main/java/com/tersesystems/echopraxia/ValueField.java
@@ -1,4 +1,11 @@
 package com.tersesystems.echopraxia;
 
-/** Indicates that the plain value should be rendered in message template. */
+/**
+ * Marker interface for key values.
+ *
+ * <p>Indicates that the plain value should be rendered in message template.
+ *
+ * <p>This marker interface is used internally, but you typically won't need to use it directly.
+ * You can call `Field.Builder.value` to get a instance of a field with this.
+ */
 public interface ValueField extends Field {}
