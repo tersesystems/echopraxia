bf611e3c919f0cc0ef9372de45393553ce729245
Will Sargent <will.sargent@gmail.com>
2022-02-04 10:39:14 -0800

    Condition should always be called after log level (#74)

--- logstash/src/main/java/com/tersesystems/echopraxia/logstash/LogstashCoreLogger.java
+++ logstash/src/main/java/com/tersesystems/echopraxia/logstash/LogstashCoreLogger.java
@@ -115,7 +115,7 @@ public class LogstashCoreLogger implements CoreLogger {
     if (condition == Condition.never()) {
       return false;
     }
-    Marker marker = context.convertMarkers();
+    Marker marker = context.getMarker();
     return logger.isEnabledFor(marker, convertLogbackLevel(level))
         && condition.test(level, context);
   }
@@ -128,19 +128,17 @@ public class LogstashCoreLogger implements CoreLogger {
     if (condition == Condition.never()) {
       return false;
     }
-    Marker marker = context.convertMarkers();
+    Marker marker = context.getMarker();
     return logger.isEnabledFor(marker, convertLogbackLevel(level))
         && this.condition.and(condition).test(level, context);
   }
 
   @Override
   public void log(@NotNull Level level, String message) {
-    if (!condition.test(level, context)) {
-      return;
+    Marker m = context.getMarker();
+    if (logger.isEnabledFor(m, convertLogbackLevel(level)) && condition.test(level, context)) {
+      logger.log(m, fqcn, convertLevel(level), message, null, null);
     }
-
-    Marker m = context.convertMarkers();
-    logger.log(m, fqcn, convertLevel(level), message, null, null);
   }
 
   @Override
@@ -149,32 +147,30 @@ public class LogstashCoreLogger implements CoreLogger {
       String message,
       Field.@NotNull BuilderFunction<FB> f,
       @NotNull FB builder) {
-    if (!condition.test(level, context)) {
-      return;
-    }
 
-    final Marker m = context.convertMarkers();
-    final List<Field> args = f.apply(builder);
-    final Object[] arguments = convertArguments(args);
-    logger.log(m, fqcn, convertLevel(level), message, arguments, null);
+    final Marker m = context.getMarker();
+    if (logger.isEnabledFor(m, convertLogbackLevel(level)) && condition.test(level, context)) {
+      final List<Field> args = f.apply(builder);
+      final Object[] arguments = convertArguments(args);
+      logger.log(m, fqcn, convertLevel(level), message, arguments, null);
+    }
   }
 
   @Override
   public void log(@NotNull Level level, String message, @NotNull Throwable e) {
-    if (!condition.test(level, context)) {
-      return;
+    final Marker m = context.getMarker();
+    if (logger.isEnabledFor(m, convertLogbackLevel(level)) && condition.test(level, context)) {
+      logger.log(m, fqcn, convertLevel(level), message, null, e);
     }
-
-    Marker m = context.convertMarkers();
-    logger.log(m, fqcn, convertLevel(level), message, null, e);
   }
 
   @Override
   public void log(@NotNull Level level, @NotNull Condition condition, String message) {
-    if (!condition.test(level, context)) {
-      return;
+    final Marker m = context.getMarker();
+    if (logger.isEnabledFor(m, convertLogbackLevel(level))
+        && this.condition.and(condition).test(level, context)) {
+      logger.log(m, fqcn, convertLevel(level), message, null, null);
     }
-    log(level, message);
   }
 
   @Override
@@ -183,10 +179,11 @@ public class LogstashCoreLogger implements CoreLogger {
       @NotNull Condition condition,
       @Nullable String message,
       @NotNull Throwable e) {
-    if (!condition.test(level, context)) {
-      return;
+    final Marker m = context.getMarker();
+    if (logger.isEnabledFor(m, convertLogbackLevel(level))
+        && this.condition.and(condition).test(level, context)) {
+      logger.log(m, fqcn, convertLevel(level), message, null, e);
     }
-    log(level, message, e);
   }
 
   @Override
@@ -196,10 +193,13 @@ public class LogstashCoreLogger implements CoreLogger {
       @Nullable String message,
       @NotNull Field.BuilderFunction<B> f,
       @NotNull B builder) {
-    if (!condition.test(level, context)) {
-      return;
+    final Marker m = context.getMarker();
+    if (logger.isEnabledFor(m, convertLogbackLevel(level))
+        && this.condition.and(condition).test(level, context)) {
+      final List<Field> args = f.apply(builder);
+      final Object[] arguments = convertArguments(args);
+      logger.log(m, fqcn, convertLevel(level), message, arguments, null);
     }
-    log(level, message, f, builder);
   }
 
   @Override
