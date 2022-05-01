01b5020259b89bd0d7e1ef557fb67c29359b1bda
Will Sargent <will.sargent@gmail.com>
2022-05-01 16:27:43 -0700

    Remove scala module

--- scala/api/src/main/scala/com/tersesystems/echopraxia/scala/api/AbstractLoggerSupport.scala
+++
@@ -1,8 +0,0 @@
-package com.tersesystems.echopraxia.scala.api
-
-import com.tersesystems.echopraxia.api.CoreLogger
-
-abstract class AbstractLoggerSupport[FB](val core: CoreLogger, val fieldBuilder: FB)
-    extends DefaultMethodsSupport[FB] {
-  def name: String = core.getName
-}
