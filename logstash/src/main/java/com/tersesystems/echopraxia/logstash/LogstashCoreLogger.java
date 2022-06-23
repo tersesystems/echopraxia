0238a48ff8897979fd570dcfc3c824d4c6e93996
Will Sargent <will.sargent@gmail.com>
2022-06-23 08:35:00 -0700

    format

--- logstash/src/main/java/com/tersesystems/echopraxia/logstash/LogstashCoreLogger.java
+++ logstash/src/main/java/com/tersesystems/echopraxia/logstash/LogstashCoreLogger.java
@@ -103,11 +103,11 @@ public class LogstashCoreLogger implements CoreLogger {
 
   @Override
   public <FB> @NotNull CoreLogger withFields(
-    @NotNull Function<FB, FieldBuilderResult> f, @NotNull FB builder) {
+      @NotNull Function<FB, FieldBuilderResult> f, @NotNull FB builder) {
     final LogstashLoggingContext contextWithFields =
-      this.context.withFields(() -> convertToFields(f.apply(builder)));
+        this.context.withFields(() -> convertToFields(f.apply(builder)));
     return new LogstashCoreLogger(
-      fqcn, logger, contextWithFields, condition, executor, threadContextFunction);
+        fqcn, logger, contextWithFields, condition, executor, threadContextFunction);
   }
 
   public CoreLogger withMarkers(Marker... markers) {
