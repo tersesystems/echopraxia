45eefb7f5365a57e68ee0737175dd8d8ad4b2a14
Will Sargent <will.sargent@gmail.com>
2022-09-03 21:42:00 -0700

    fix tests

--- logback/src/main/java/com/tersesystems/echopraxia/logback/FieldMarker.java
+++ logback/src/main/java/com/tersesystems/echopraxia/logback/FieldMarker.java
@@ -1,24 +1,26 @@
 package com.tersesystems.echopraxia.logback;
 
 import com.tersesystems.echopraxia.api.Field;
+import com.tersesystems.echopraxia.api.FieldBuilderResult;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 /** A marker that contains an echopraxia field. */
 public class FieldMarker extends BaseMarker {
 
-  private final Field field;
+  private final FieldBuilderResult result;
 
-  FieldMarker(@NotNull Field field) {
+  FieldMarker(@NotNull FieldBuilderResult field) {
     super(field.toString());
-    this.field = field;
+    this.result = field;
   }
 
   @NotNull
-  public static FieldMarker apply(@NotNull Field field) {
+  public static FieldMarker apply(@NotNull FieldBuilderResult field) {
     return new FieldMarker(field);
   }
 
-  public Field getField() {
-    return field;
+  public List<Field> getFields() {
+    return result.fields();
   }
 }
--- logstash/src/main/java/com/tersesystems/echopraxia/logstash/LogstashFieldAppender.java
+++ logstash/src/main/java/com/tersesystems/echopraxia/logstash/LogstashFieldAppender.java
@@ -5,7 +5,13 @@ import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.classic.spi.ThrowableProxy;
 import com.tersesystems.echopraxia.api.Field;
 import com.tersesystems.echopraxia.api.Value;
+import com.tersesystems.echopraxia.logback.FieldMarker;
 import com.tersesystems.echopraxia.logback.TransformingAppender;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import net.logstash.logback.marker.Markers;
+import org.slf4j.Marker;
 
 /**
  * An appender that converts the logging event from containing Field into containing
@@ -17,24 +23,49 @@ public class LogstashFieldAppender extends TransformingAppender<ILoggingEvent>
     implements LogstashFieldConverter {
   @Override
   protected ILoggingEvent decorateEvent(ILoggingEvent eventObject) {
+    // Run through the markers and convert FieldMarker to LogstashMarker
+    final Marker marker = eventObject.getMarker();
+    if (marker != null) {
+      List<Marker> markers = new ArrayList<>();
+      if (marker instanceof FieldMarker) {
+        final List<Field> fields = ((FieldMarker) marker).getFields();
+        for (Field field : fields) {
+          markers.add(convertMarker(field));
+        }
+      }
+      final Iterator<Marker> iterator = marker.iterator();
+      for (Marker m; iterator.hasNext(); ) {
+        m = iterator.next();
+        if (m instanceof FieldMarker) {
+          final List<Field> fields = ((FieldMarker) m).getFields();
+          for (Field field : fields) {
+            markers.add(convertMarker(field));
+          }
+        }
+      }
+      // Add the original marker to the end of the list...
+      marker.add(Markers.aggregate(markers));
+    }
+
     // what comes in is fields, what comes out is structured arguments
     final Object[] argumentArray = eventObject.getArgumentArray();
-    for (int i = 0; i < argumentArray.length; i++) {
-      final Object arg = argumentArray[i];
-      if (arg instanceof Field) {
-        Field field = (Field) arg;
-        argumentArray[i] = convertArgument(field);
+    if (argumentArray != null) {
+      for (int i = 0; i < argumentArray.length; i++) {
+        final Object arg = argumentArray[i];
+        if (arg instanceof Field) {
+          Field field = (Field) arg;
+          argumentArray[i] = convertArgument(field);
 
-        // swap out the throwable if one is found
-        if (eventObject.getThrowableProxy() == null) {
-          final Throwable throwable = extractThrowable(field);
-          if (throwable != null) {
-            ((LoggingEvent) eventObject).setThrowableProxy(new ThrowableProxy(throwable));
+          // swap out the throwable if one is found
+          if (eventObject.getThrowableProxy() == null) {
+            final Throwable throwable = extractThrowable(field);
+            if (throwable != null) {
+              ((LoggingEvent) eventObject).setThrowableProxy(new ThrowableProxy(throwable));
+            }
           }
         }
       }
     }
-
     return eventObject;
   }
 
