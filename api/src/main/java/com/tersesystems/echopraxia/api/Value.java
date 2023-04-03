1b8b7ebdd5da1e8c16791576f8efe06199b83a45
Will Sargent <will.sargent@gmail.com>
2023-04-02 22:02:46 -0700

    Add rich values (#237)

--- api/src/main/java/com/tersesystems/echopraxia/api/Value.java
+++ api/src/main/java/com/tersesystems/echopraxia/api/Value.java
@@ -44,6 +44,45 @@ public abstract class Value<V> {
   @NotNull
   public abstract Value.Type type();
 
+  /** @return this value as an object value. */
+  @NotNull
+  public ObjectValue asObject() {
+    return (ObjectValue) this;
+  }
+
+  /** @return this value as an array value */
+  @NotNull
+  public ArrayValue asArray() {
+    return (ArrayValue) this;
+  }
+
+  /** @return this value as a string value */
+  @NotNull
+  public StringValue asString() {
+    return (StringValue) this;
+  }
+
+  /** @return this value as a number value */
+  @NotNull
+  public <T extends Number & Comparable<T>> NumberValue<T> asNumber() {
+    return (NumberValue<T>) this;
+  }
+
+  /** @return this value as an exception value */
+  public ExceptionValue asException() {
+    return (ExceptionValue) this;
+  }
+
+  /** @return this value as a null value */
+  public NullValue asNull() {
+    return (NullValue) this;
+  }
+
+  /** @return this value as a boolean value */
+  public BooleanValue asBoolean() {
+    return (BooleanValue) this;
+  }
+
   @NotNull
   public String toString() {
     final Object raw = raw();
@@ -685,6 +724,18 @@ public abstract class Value<V> {
       return Type.ARRAY;
     }
 
+    public ArrayValue add(Value<?> value) {
+      ArrayList<Value<?>> values = new ArrayList<>(this.raw);
+      values.add(value);
+      return new ArrayValue(values);
+    }
+
+    public ArrayValue addAll(Collection<Value<?>> values) {
+      ArrayList<Value<?>> joinedValues = new ArrayList<>(this.raw);
+      joinedValues.addAll(values);
+      return new ArrayValue(joinedValues);
+    }
+
     @Override
     public boolean equals(Object o) {
       if (this == o) return true;
@@ -716,6 +767,18 @@ public abstract class Value<V> {
     @Override
     public List<Field> raw() {
       return raw;
+    }
+
+    public ObjectValue add(Field field) {
+      ArrayList<Field> fields = new ArrayList<>(this.raw);
+      fields.add(field);
+      return new ObjectValue(fields);
+    }
+
+    public ObjectValue addAll(Collection<Field> fields) {
+      ArrayList<Field> joinedFields = new ArrayList<>(this.raw);
+      joinedFields.addAll(fields);
+      return new ObjectValue(joinedFields);
     }
 
     @Override
---
+++ api/src/test/java/com/tersesystems/echopraxia/api/ValueTests.java
@@ -0,0 +1,47 @@
+package com.tersesystems.echopraxia.api;
+
+import static java.util.Collections.singleton;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ValueTests {
+
+  @Test
+  void testObjectValueAdd() {
+    Value.ObjectValue object = Value.object();
+    Value.StringValue stringValue = Value.string("some string value");
+    Field stringField = Field.value("string", stringValue);
+    Value.ObjectValue objectPlus = object.add(stringField);
+    assertThat(objectPlus.raw().size()).isEqualTo(1);
+  }
+
+  @Test
+  void testObjectValueAddAll() {
+    Value.ObjectValue object = Value.object();
+    Value.StringValue stringValue = Value.string("some string value");
+    Field stringField = Field.value("string", stringValue);
+    Value.ObjectValue objectPlus = object.addAll(singleton(stringField));
+    assertThat(objectPlus.raw().size()).isEqualTo(1);
+  }
+
+  @Test
+  void testArrayValueAdd() {
+    Value.StringValue stringValue = Value.string("some string value");
+    Value.NumberValue<Integer> numberValue = Value.number(1);
+    Value.ArrayValue arrayValue = Value.array(stringValue);
+
+    Value.ArrayValue arrayPlus = arrayValue.add(numberValue);
+    assertThat(arrayPlus.raw().size()).isEqualTo(2);
+  }
+
+  @Test
+  void testArrayValueAddAll() {
+    Value.StringValue stringValue = Value.string("some string value");
+    Value.NumberValue<Integer> numberValue = Value.number(1);
+    Value.ArrayValue arrayValue = Value.array(stringValue);
+
+    Value.ArrayValue arrayPlus = arrayValue.addAll(singleton(numberValue));
+    assertThat(arrayPlus.raw().size()).isEqualTo(2);
+  }
+}
