85b4a590f10057d01447970c6a063a523644616f
Will Sargent <will.sargent@gmail.com>
2023-04-08 11:57:42 -0700

    Remove SPI callout to StaticLoggerBinder

--- logstash/src/main/java/com/tersesystems/echopraxia/logstash/LogstashLoggerProvider.java
+++ logstash/src/main/java/com/tersesystems/echopraxia/logstash/LogstashLoggerProvider.java
@@ -3,16 +3,10 @@ package com.tersesystems.echopraxia.logstash;
 import ch.qos.logback.classic.LoggerContext;
 import com.tersesystems.echopraxia.api.CoreLogger;
 import com.tersesystems.echopraxia.api.CoreLoggerProvider;
+import java.util.ServiceConfigurationError;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Iterator;
-import java.util.Optional;
-import java.util.ServiceConfigurationError;
-import java.util.ServiceLoader;
 
 /**
  * Logstash implementation of a logger provider.
@@ -23,7 +17,7 @@ public class LogstashLoggerProvider implements CoreLoggerProvider {
 
   // Customize the number of retries, using 10 as the default
   private static final int retryCount =
-          Integer.parseInt(System.getProperty("echopraxia.logback.retries", "10"));
+      Integer.parseInt(System.getProperty("echopraxia.logback.retries", "10"));
 
   private volatile LoggerContext loggerContext;
 
@@ -53,7 +47,7 @@ public class LogstashLoggerProvider implements CoreLoggerProvider {
               break;
             } else {
               System.err.println(
-                      "LogstashLoggerProvider: Logback still initializing, sleeping for 100 ms...");
+                  "LogstashLoggerProvider: Logback still initializing, sleeping for 100 ms...");
               retries -= 1;
               // https://logback.qos.ch/manual/configuration.html#auto_configuration
               // It takes about 100 milliseconds for Joran to parse a given logback configuration
@@ -66,7 +60,7 @@ public class LogstashLoggerProvider implements CoreLoggerProvider {
         }
         if (loggerContext == null) {
           System.err.println(
-                  "LogstashLoggerProvider: No Logback implementation can be found after 10 retries.  Giving up.");
+              "LogstashLoggerProvider: No Logback implementation can be found after 10 retries.  Giving up.");
           throw new ServiceConfigurationError("Invalid ILoggerFactory implementation " + factory);
         }
       }
