3f42dd002f0fd66a8db49c2ccf1d2b577df84c09
Will Sargent <will.sargent@gmail.com>
2023-05-23 11:13:25 -0700

    Add abbreviateAfter

---
+++ api/src/test/java/com/tersesystems/echopraxia/api/AbbreviationTests.java
@@ -0,0 +1,37 @@
+package com.tersesystems.echopraxia.api;
+
+import static com.tersesystems.echopraxia.api.FieldAttributes.abbreviateAfter;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AbbreviationTests {
+
+  @Test
+  public void abbreviateString() {
+    Value<String> value = Value.string("123456789");
+    Field field = Field.keyValue("longString", value).withAttribute(abbreviateAfter(5));
+    assertThat(field.toString()).isEqualTo("longString=12345...");
+  }
+
+  @Test
+  public void abbreviateShortString() {
+    Value<String> value = Value.string("12345");
+    Field field = Field.keyValue("longString", value).withAttribute(abbreviateAfter(5));
+    assertThat(field.toString()).isEqualTo("longString=12345");
+  }
+
+  @Test
+  public void abbreviateArray() {
+    Value<?> value = Value.array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    Field field = Field.keyValue("longArray", value).withAttribute(abbreviateAfter(5));
+    assertThat(field.toString()).isEqualTo("longArray=[1, 2, 3, 4, 5...]");
+  }
+
+  @Test
+  public void abbreviateShortArray() {
+    Value<?> value = Value.array(1, 2, 3, 4, 5);
+    Field field = Field.keyValue("longArray", value).withAttribute(abbreviateAfter(5));
+    assertThat(field.toString()).isEqualTo("longArray=[1, 2, 3, 4, 5]");
+  }
+}
