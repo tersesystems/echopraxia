72c06a771bed6bfb403b1ecc1490cd8dcb042687
Will Sargent <will.sargent@gmail.com>
2024-04-13 12:08:17 -0700

    Update with string value

--- api/src/test/java/com/tersesystems/echopraxia/api/ToStringValueTests.java
+++ api/src/test/java/com/tersesystems/echopraxia/api/ToStringValueTests.java
@@ -12,12 +12,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ToStringValueTests {
 
     @Test
-    public void testDurationValue() {
+    public void testString() {
         Duration duration = Duration.ofDays(1);
         var value = Value.string(duration.toString()); // PT24H in line and JSON
         Attribute<String> stringAttribute = withToStringValue(formatDuration(duration));
         var durationWithToString = value.withAttributes(Attributes.create(stringAttribute)); // 1 day in toString()
         assertThat(durationWithToString).hasToString("1 day");
+    }
+
+    @Test
+    public void testArray() {
+        var one = Value.string("one");
+        var two = Value.string("two").withAttributes(Attributes.create(withToStringValue("dos")));
+        var three = Value.string("three");
+        var array = Value.array(one, two, three);
+        assertThat(array).hasToString("[one, dos, three]");
     }
 
     private static String formatDuration(Duration duration) {
