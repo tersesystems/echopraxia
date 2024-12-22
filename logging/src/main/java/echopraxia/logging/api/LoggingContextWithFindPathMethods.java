83537f0b3b1b414d4c9a16d5a944f66daa2193cd
Will Sargent <will.sargent@gmail.com>
2024-12-21 18:37:52 -0800

    add doc

--- logging/src/main/java/echopraxia/logging/api/LoggingContextWithFindPathMethods.java
+++ logging/src/main/java/echopraxia/logging/api/LoggingContextWithFindPathMethods.java
@@ -2,4 +2,5 @@ package echopraxia.logging.api;
 
 import echopraxia.api.FindPathMethods;
 
+/** An extended logging context with JSON find methods. */
 public interface LoggingContextWithFindPathMethods extends LoggingContext, FindPathMethods {}
